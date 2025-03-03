5ed865e0e4810674cb93f9a99586aa9765bf4c49
Jan Ulrichts <jan.ulrichts@gmail.com>
2025-03-03 13:29:22 +0100

    Added epsilon (empty string) type and used single line string to further constrain CFG part construction.

--- src/CFG.Parsing/ContextFreeGrammar.fs
+++ src/CFG.Parsing/ContextFreeGrammar.fs
@@ -1,12 +1,16 @@
 ﻿namespace CFG.Parsing
 
+open SingleLineString
+
 module ContextFreeGrammar =
-    type Literal = Literal of string
-    type IdentifierPart = IdentifierPart of string
+    type Epsilon = Epsilon
+    type Literal = Literal of SingleLineString
+    type IdentifierPart = IdentifierPart of SingleLineString
     type Identifier = Identifier of IdentifierPart list
     type DefinitionPart =
         | LiteralDefinitionPart of Literal
         | IdentifierDefinitionPart of Identifier
+        | EpsilonDefinitionPart of Epsilon
     type DefinitionChoice = DefinitionChoice of DefinitionPart list
     type Definition = Definition of DefinitionChoice list
     type Rule = {
